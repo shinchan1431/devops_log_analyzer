32c218d70f088b5141b4cee8aad56fb1e30d2c06
shinsu <ajithkumar472004@gmail.com>
2026-08-25 22:13:38 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: 'devops_log_analyzer',
+  base: '/devops_log_analyzer/',
   plugins: [react()],
   server: {
     host: '0.0.0.0',
